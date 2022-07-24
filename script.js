8855524ac11313ceda9a0c1a26a5ccc967a4e3f2
Helena2018 <helena2020liu@gmail.com>
2022-07-24 15:12:01 +1200

    start picture in picture

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ async function selectMediaStream() {
 button.addEventListener('click', async () => {
   // Disable button
   button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
   
 });
 
