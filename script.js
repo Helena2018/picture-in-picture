747485e5d0610fd6b9045372fa100e787d897474
Helena2018 <helena2020liu@gmail.com>
2022-07-24 15:07:18 +1200

    pass to video element, then play

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
+button.addEventListener('click', selectMediaStream)
+
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
   try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
-    console.log(mediaStream)
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play(); 
+    }
   } catch(error) {
     // Catch Error Here
     console.log('whoooops, error here: ', error);
