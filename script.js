42acc827b556b4e1efd81df484846a5ebe264f7e
Helena2018 <helena2020liu@gmail.com>
2022-07-24 15:00:01 +1200

    set media stream

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const videoElement = document.getElementById('video')
-const button = document.getElementById('button')
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+    console.log(mediaStream)
+  } catch(error) {
+    // Catch Error Here
+    console.log('whoooops, error here: ', error);
+  }
+}
+
+// On Load 
+selectMediaStream();
